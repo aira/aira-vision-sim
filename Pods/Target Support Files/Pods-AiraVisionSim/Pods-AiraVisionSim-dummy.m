bf596c5ae6278256fcc577208c77cfe38dc0f95d
Hsanka6 <haasith2@gmail.com>
2017-08-29 15:53:57 -0700

    removed unneccessary files

--- Pods/Target Support Files/Pods-AiraVisionSim/Pods-AiraVisionSim-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AiraVisionSim : NSObject
-@end
-@implementation PodsDummy_Pods_AiraVisionSim
-@end
--- Pods/Target Support Files/Pods-AiraVisionSimTests/Pods-AiraVisionSimTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AiraVisionSimTests : NSObject
-@end
-@implementation PodsDummy_Pods_AiraVisionSimTests
-@end
